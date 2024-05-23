d61e44d1767f3d18258f5bf73bd3decfed60491a
Ritoban Goswami <dev.ritoban.goswami@gmail.com>
2024-05-23 21:47:15 +0530

    feat: removed social links from pageInfo schema

--- schemas/pageInfo.ts
+++ schemas/pageInfo.ts
@@ -46,11 +46,5 @@ export default defineType({
       type: 'array',
       of: [{type: 'reference', to: {type: 'projects'}}],
     }),
-    defineField({
-      name: 'socialLinks',
-      title: 'Social Links',
-      type: 'array',
-      of: [{type: 'reference', to: {type: 'links'}}],
-    }),
   ],
 })
